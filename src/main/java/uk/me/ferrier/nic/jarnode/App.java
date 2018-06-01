286ee409a4f6dcd00be1b726f7123739785ea8ee
Nic Ferrier <nic@ferrier.me.uk>
2018-06-01 23:35:39 +0100

    allow exe permissions to be placed after

--- src/main/java/uk/me/ferrier/nic/jarnode/App.java
+++ src/main/java/uk/me/ferrier/nic/jarnode/App.java
@@ -8,10 +8,18 @@ import java.util.jar.JarEntry;
 import java.util.Enumeration;
 import java.io.IOException;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.FileVisitResult;
+import java.nio.file.attribute.BasicFileAttributes;
 
 /**
  * Boot node apps by reading them out of the uberjar.
@@ -19,7 +27,6 @@ import java.io.InputStreamReader;
  */
 public class App
 {
-
     static class Entry {
         List<String> list;
         String name;
@@ -30,25 +37,46 @@ public class App
         }
     }
 
-    public static void main(String[] args) throws IOException
-    {
-        File tempDir = File.createTempFile("nodeapp", Long.toString(System.nanoTime()));
+    static File getTargetDir(String sourceJarPath) {
+        Path sourceJar = FileSystems.getDefault().getPath(sourceJarPath);
+        Path dir = sourceJar.getParent();
+        Path name = sourceJar.getFileName();
+        File f = new File(dir.toFile(), "." + name);
+        return f;
+    }
 
-        if(! (tempDir.delete())) {
-            throw new IOException("Could not delete temp file: " + tempDir.getAbsolutePath());
+    static File rmTargetDir(String sourceJarPath) {
+        File pathName = getTargetDir(sourceJarPath);
+        Path directory = FileSystems.getDefault().getPath(pathName.toString());
+        try {
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        Files.delete(file);
+                        return FileVisitResult.CONTINUE;
+                    }
+                    
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
         }
-
-        if(! (tempDir.mkdir())) {
-            throw new IOException("Could not create temp directory: " + tempDir.getAbsolutePath());
+        catch (IOException e) {
+            // swallow
         }
+        return directory.toFile();
+    }
 
-        System.out.println("nodeapp - " + tempDir.getAbsolutePath());
+    static File makeTargetDir(String sourceJarPath) {
+        File directory = rmTargetDir(sourceJarPath);
+        boolean created = directory.mkdirs();
+        return directory;
+    }
 
-        String path = App.class
-            .getProtectionDomain()
-            .getCodeSource()
-            .getLocation()
-            .getPath();
+    static File extractJar(String path) throws IOException {
+        File tempDir = makeTargetDir(path);
         JarFile jar = new JarFile(path);
         Enumeration<JarEntry> entries = jar.entries();
         while (entries.hasMoreElements()) {
@@ -72,6 +100,33 @@ public class App
                 }
             }
         }
+        return tempDir;
+    }
+
+    static void fixPerms(File jarDir) throws IOException {
+        // If there is a list of exe files then we must apply that
+        File exeList = new File(jarDir, ".exethings");
+        if (exeList.exists()) {
+            char[] buf = new char[(int)exeList.length()];
+            new FileReader(exeList).read(buf);
+            String[] list = new String(buf).split("\n");
+            for (String fileName : list) {
+                File file = new File(jarDir, fileName.trim());
+                file.setExecutable(true);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        String path = App.class
+            .getProtectionDomain()
+            .getCodeSource()
+            .getLocation()
+            .getPath();
+
+        File nodeAppJarDir = extractJar(path);
+        fixPerms(nodeAppJarDir);
 
         // Find node in the PATH
         String OS = System.getProperty("os.name");
@@ -94,7 +149,7 @@ public class App
         String nodeExe = nodePath.get(0).name + "/node";
         nodeExe = isWin? nodeExe + ".exe" : nodeExe;
         ProcessBuilder builder = new ProcessBuilder(nodeExe, "server.js");
-        builder.directory(tempDir);
+        builder.directory(nodeAppJarDir);
         builder.redirectErrorStream(true);
         Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
--- src/test/java/uk/me/ferrier/nic/AppTest.java
+++ src/test/java/uk/me/ferrier/nic/AppTest.java
@@ -4,6 +4,13 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import java.util.Arrays;
+
 /**
  * Unit test for simple App.
  */
@@ -29,10 +36,48 @@ public class AppTest
     }
 
     /**
-     * Rigourous Test :-)
+     * Test the file creation and destruction logic.
      */
-    public void testApp()
+    public void testApp() throws IOException
     {
-        assertTrue( true );
+        String targetJar = new File("./somejar.jar").getCanonicalPath();
+
+        // Make fake path
+        File f = App.getTargetDir(targetJar);
+        File path = new File(new File(f, "level1"), "level2");
+        path.mkdirs();
+
+        File targetDir = App.rmTargetDir(targetJar);
+        assertTrue( !targetDir.exists() ); // should have been rm'd
+
+        targetDir = App.makeTargetDir(targetJar);
+        assertTrue( targetDir.exists() );
+    }
+
+    public void testExtract() throws Exception {
+        String jarPath = new File("src/test/resources/demojar.jar")
+            .getCanonicalPath();
+        File extractJar = App.extractJar(jarPath);
+        File[] list = extractJar.listFiles();
+        // for (File f : list) System.out.println("list member " + f);
+        File jarPathDir = new File(jarPath).getParentFile();
+        File serverJsPath = new File(jarPathDir, ".demojar.jar/server.js");
+        // System.out.println("serverJsPath " + serverJsPath);
+        assertTrue( Arrays
+                    .asList(list)
+                    .contains(serverJsPath) );
+    }
+
+    public void testPerms() throws Exception {
+        String jarPath = new File("src/test/resources/demojar.jar")
+            .getCanonicalPath();
+        File extractJar = App.extractJar(jarPath);
+        App.fixPerms(extractJar);
+
+        String testExeFile = "node_modules/mkdirp/bin/cmd.js";
+        File jarPathDir = new File(jarPath).getParentFile();
+        File exePath = new File(jarPathDir, ".demojar.jar/" + testExeFile);
+        boolean isExe = exePath.canExecute();
+        assertTrue( isExe );
     }
 }
