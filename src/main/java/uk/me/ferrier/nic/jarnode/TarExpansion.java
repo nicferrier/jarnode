c240ee3e59259a42cf21dd276896dffb4a94e8f6
Nic Ferrier <nic@ferrier.me.uk>
2018-08-02 17:23:44 +0100

    beginnings of testing whether delivery of node enviroments works

---
+++ src/main/java/uk/me/ferrier/nic/jarnode/TarExpansion.java
@@ -0,0 +1,63 @@
+package uk.me.ferrier.nic.jarnode;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.Set;
+
+import org.tukaani.xz.XZInputStream;
+import org.kamranzafar.jtar.TarInputStream;
+import org.kamranzafar.jtar.TarEntry;
+import org.kamranzafar.jtar.TarHeader;
+
+
+public class TarExpansion {
+    public static void expandTar(InputStream tarXzIn, String destDir) throws Exception {
+        InputStream i2 = new XZInputStream(tarXzIn);
+        InputStream i3 = new BufferedInputStream(i2);
+        TarInputStream tis = new TarInputStream(i3);
+        TarEntry entry;
+        
+        while((entry = tis.getNextEntry()) != null) {
+            int count;
+            byte data[] = new byte[2048];
+
+            String entryName = entry.getName();
+            File f = new File(destDir, entryName);
+            boolean isCreated = f.getParentFile().mkdirs();
+
+            if (!entry.isDirectory() && !f.getName().equals(".")) {
+                FileOutputStream fos = new FileOutputStream(f);
+                BufferedOutputStream dest = new BufferedOutputStream(fos);
+                
+                while((count = tis.read(data)) != -1) {
+                    dest.write(data, 0, count);
+                }
+                
+                dest.flush();
+                dest.close();
+
+                int mode = entry.getHeader().mode;
+                boolean userExecute = (mode & 0700) == 0700;
+                boolean groupExecute = (mode & 070) == 070;
+                boolean otherExecute = (mode & 07) == 07;
+
+                Path path = f.toPath();
+                Set<PosixFilePermission> set = Files.getPosixFilePermissions(path);
+                if (userExecute) set.add(PosixFilePermission.OWNER_EXECUTE);
+                if (groupExecute) set.add(PosixFilePermission.GROUP_EXECUTE);
+                if (otherExecute) set.add(PosixFilePermission.OTHERS_EXECUTE);
+                Files.setPosixFilePermissions(path, set);
+            }
+        }
+  
+        tis.close();
+    }
+}
